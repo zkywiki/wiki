bdfba0a6d2229d845f20f8b69be0fff6fb095ea2
zkywiki <323091265+zkywiki@users.noreply.github.com>
2026-09-15 23:16:40 +0900

    feat: 즈키쿠 여담 > 아즈망가대왕 및 종합게임 마추기 아이오 항목 추가

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -74,7 +74,7 @@ export const REPO = {
 export const DOCS = {
   zky: {
     title: "즈키쿠",
-    updated: "2026-09-14 01:02",
+    updated: "2026-09-15 23:16",
     file: "docs/zky.html",
     related: ["cuzky", "concert", "cupotify", "fanart"], // 관련 문서 박스
     shortcuts: [
