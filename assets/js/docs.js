f04daa6aa24126ffd79ab98e588d0a86d8a89c5e
zkywiki <323091265+zkywiki@users.noreply.github.com>
2026-09-18 15:58:36 +0900

    feat: 쿠린이콘서트 - 누락 곡 추가 보여줄게 랭커 스나

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -131,7 +131,7 @@ export const DOCS = {
 
   cupotify: {
     title: "쿠포티파이",
-    updated: "2026-09-17 22:02",
+    updated: "2026-09-18 15:57",
     file: "docs/cupotify.html",
     related: ["zky"],
     shortcuts: [
@@ -144,7 +144,7 @@ export const DOCS = {
 
   concert: {
     title: "쿠린이 콘서트",
-    updated: "2026-09-18 00:39",
+    updated: "2026-09-18 15:57",
     file: "docs/concert.html",
     related: ["zky", "cupotify"],
     shortcuts: [
