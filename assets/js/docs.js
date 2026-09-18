1ef7476a9d401adc1e8446687c3c249111910a79
zkywiki <323091265+zkywiki@users.noreply.github.com>
2026-09-19 04:03:06 +0900

    feat: 즈키쿠/종합게임 The Death Game Report, 나오렴 고양아 추가

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -98,7 +98,7 @@ export const REPO = {
 export const DOCS = {
   zky: {
     title: "즈키쿠",
-    updated: "2026-09-18 16:03",
+    updated: "2026-09-19 04:03",
     file: "docs/zky.html",
     related: ["cuzky", "concert", "cupotify", "fanart"], // 관련 문서 박스
     shortcuts: [
